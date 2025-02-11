ec23b32ce7c789d0873d27f2999ca50558f8e78d
ilya-t <extraletters@gmail.com>
2025-02-11 17:02:56 +0300

    tiny fixes

--- streamer/app/entrypoint.sh
+++ streamer/app/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
-SAMPLE=/app/sample.mp4
-if [ ! -f $SAMPLE ]; then
+TARGET=127.0.0.1
+# TARGET=192.168.1.232
+PORT=12345
+SAMPLE=./sample.mp4
+
+if [ ! -f "$SAMPLE" ]; then
     echo "File $SAMPLE does not exist!"
     exit 1
 fi
@@ -11,6 +15,7 @@ trap "echo 'Exiting...'; exit" SIGINT
 echo "Will playback: $SAMPLE in loop!"
 
 playback() {
+    echo "====> Starting playback to $TARGET:$PORT!"
     gst-launch-1.0 -v \
         filesrc location=$SAMPLE ! \
         qtdemux name=demux demux.video_0 ! \
@@ -24,10 +29,10 @@ if [ -z "$NO_LOOP" ]; then
     while true; do
         playback
         echo "Playback completed, will restart in a second..."
+        echo "==============================================="
         sleep 1
     done
 else
     playback
     echo "Playback completed."
 fi
-done
