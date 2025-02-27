c8d3a7a933cb446ef25e16a5d958895d2664bb01
ilya-t <extraletters@gmail.com>
2025-02-27 23:04:38 +0300

    remove debug code

--- control_app/app/src/main/java/rc/playgrounds/stream/StreamingProcess.kt
+++ control_app/app/src/main/java/rc/playgrounds/stream/StreamingProcess.kt
@@ -1,13 +1,8 @@
 package rc.playgrounds.stream
 
-import a.debug.stuff.ControlPanel
 import android.annotation.SuppressLint
-import android.net.Uri
-import android.view.SurfaceHolder
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import com.testspace.core.Static
-import org.freedesktop.gstreamer.GStreamerSurfaceView
 import rc.playgrounds.config.ConfigModel
 
 class StreamingProcess(
@@ -15,21 +10,6 @@ class StreamingProcess(
     private val activity: AppCompatActivity,
     private val surfaceContainer: ViewGroup,
 ) {
-    init {
-        ControlPanel.setup(activity)
-            .trigger("release") {
-                streamReceiver.release()
-                ControlPanel.setup(activity).print("RELEASED!")
-            }
-            .trigger("create") {
-                streamReceiver = createReceiver()
-                ControlPanel.setup(activity).print("CREATED!")
-            }
-            .trigger("play") {
-                streamReceiver.play()
-                ControlPanel.setup(activity).print("PLAYING!")
-            }
-    }
     private var streamReceiver = createReceiver()
 
     @SuppressLint("UnsafeOptInUsageError")
