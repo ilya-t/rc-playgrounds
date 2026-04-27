d8971277232260a1738e8ef0e87178d079333b19
ilya-t <extraletters@gmail.com>
2026-04-27 21:27:57 +0300

    bump version v.0.4

--- control_app/app/build.gradle.kts
+++ control_app/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         minSdkVersion(Dependencies.minSdkVersion)
         compileSdkVersion(Dependencies.targetSdkVersion)
         targetSdkVersion(Dependencies.targetSdkVersion)
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
